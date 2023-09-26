919afa0b4fcffcefcc24778eadb417dc5fec94ec
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-09-26 11:22:48 +0300

    feat:🎸 add new commig

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let count = 0
+    $: double = count + 2;
+
+    function increment() {
+        count += 1;
+    }
+</script>
+
+<button on:mouseenter={increment}>
+	clicks: {count}
+</button>
+<a href="/dashboard">About my site</a>
+
